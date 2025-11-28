3238a4e1a8c50a66ec2d2ca922fed1ce1f24a93d
diwasbk <diwasbk903@gmail.com>
2025-11-28 12:59:53 +0545

    Get User by Id

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,4 +16,29 @@ export class UserController {
             })
         }
     }
+
+    // Get User by Id
+    getUserById = (req, res) => {
+        try {
+            const existingUser = users.find((user) => {
+                return user.id == req.params.id
+            })
+            if (!existingUser) {
+                return res.status(404).send({
+                    message: "User not found",
+                    success: false
+                })
+            }
+            res.status(200).send({
+                message: `Welcome ${existingUser.name}`,
+                result: existingUser,
+                success: true
+            })
+        } catch (err) {
+            res.send({
+                message: err.message ?? "Unknown error",
+                success: false
+            })
+        }
+    }
 };
