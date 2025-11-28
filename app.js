1ef90c7307c5b0d2d8c4c8e4fedcf3bc1c253408
diwasbk <diwasbk903@gmail.com>
2025-11-29 01:33:15 +0545

    Enable CORS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import userRouter from "./routes/userRouter.js"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/user", userRouter)
 
