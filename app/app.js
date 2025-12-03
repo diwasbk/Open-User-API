07e804cfb9d1b6d2887bc19ee62449fe449deb50
diwasbk <diwasbk903@gmail.com>
2025-12-04 02:05:02 +0545

    Dockerized

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 import express from "express"
 import userRouter from "./routes/userRouter.js"
 import cors from "cors"
+import dotenv from "dotenv"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+dotenv.config()
 
 app.use("/api/user", userRouter)
 
-app.listen(4200, () => {
-    console.log("Server is running at the port 4200.")
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running at the port ${PORT}.`)
 })
